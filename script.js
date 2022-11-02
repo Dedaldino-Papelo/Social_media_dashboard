08d156f115bf63d21ab2e7e56ce4a418fbade18c
Dedalino <kelsonpapelo18@gmail.com>
2022-11-03 00:36:25 +0100

    change the text to dark and light mode respectively

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const toogler = document.getElementById('toogler-circle')
 const tooglerBg = document.getElementById('toogler')
+const Theme_Name = document.getElementById("ThemeName")
 
 const body = document.body
 
@@ -7,9 +8,11 @@ tooglerBg.addEventListener("click", () => {
     if(body.classList.contains('dark')){
         body.classList.replace("dark","light")
         toogler.style.transform = "translateX(20px)"
+        Theme_Name.textContent = "Light Mode"
         
     } else {
         body.classList.replace("light","dark")
         toogler.style.transform = "translateX(0)"
+        Theme_Name.textContent = "Dark Mode"
     }
 })
